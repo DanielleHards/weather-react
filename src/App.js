6a9ab26ddbff6059d58720af30162056fe01f68a
DanielleHards <124166684+DanielleHards@users.noreply.github.com>
2023-08-31 20:08:50 -0400

    added github repo link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
         <h1>Weather App🌈</h1>
         <Search />
       </header>
+      <h3>
+        This project is open-sourced and can be found at my{" "}
+        <a href="https://github.com/DanielleHards/weather-react" alt="">
+          GitHub Repository
+        </a>
+      </h3>
     </div>
   );
 }
